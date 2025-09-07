69ee5081b6443fce6a311ec11ff9a51588e8f394
M946i <115243792+M946i@users.noreply.github.com>
2025-09-08 01:48:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logcb = () => () => {}
 const errcb = () => () => {}
 
 const ppua = '6a3b62e21368494db4d77d557c53baaa'
-const port = process.env.PORT||58109
+const port = 50110||58109||58111||58112
 const wss = new WebSocket.Server({ port }, logcb('listen:', port))
 
 wss.on('connection', (ws) => {
