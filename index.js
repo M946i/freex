7de908ad3a3fa8b2d26203241893e374ec5e56ab
M946i <115243792+M946i@users.noreply.github.com>
2025-09-08 02:08:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logcb = () => () => {}
 const errcb = () => () => {}
 
 const ppua = '6a3b62e21368494db4d77d557c53baaa'
-const port = process.env.LEANCLOUD_APP_PORT||7860
+const port = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000)
 const wss = new WebSocket.Server({ port }, logcb('listen:', port))
 
 wss.on('connection', (ws) => {
